6c126d7c3cd12643b0ad9f29c670a9a7e77e1114
Mason Marche <masonmarche@gmail.com>
2019-02-27 12:48:55 -0500

    working image collection

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import org.opencv.core.Mat;
-
-public class Main {
-
-    public static void main(String [] args) {
-        InstagramCollector c = new InstagramCollector("pantone");
-        System.out.println(c.getHTML());
-    }
-}
